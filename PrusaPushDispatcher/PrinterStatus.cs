7d569e9adff24e9839359f5e5bb3a651bf6a169f
Rick Gray <rickdgray@outlook.com>
2024-02-09 17:04:54 -0600

    setting up push notifications and statuses

---
+++ PrusaPushDispatcher/PrinterStatus.cs
@@ -0,0 +1,79 @@
+﻿namespace PrusaPushDispatcher
+{
+    internal class PrinterStatus
+    {
+        // See here for openapi spec
+        // https://raw.githubusercontent.com/prusa3d/Prusa-Link-Web/master/spec/openapi.yaml
+
+        private PrinterStatus(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+
+        public static PrinterStatus FromValue(string value)
+        {
+            return new PrinterStatus(value);
+        }
+
+        public static PrinterStatus Unknown { get { return new PrinterStatus("UNKNOWN"); } }
+
+        public static PrinterStatus Idle { get { return new PrinterStatus("IDLE"); } }
+        public static PrinterStatus Busy { get { return new PrinterStatus("BUSY"); } }
+        public static PrinterStatus Printing { get { return new PrinterStatus("PRINTING"); } }
+        public static PrinterStatus Paused { get { return new PrinterStatus("PAUSED"); } }
+        public static PrinterStatus Finished { get { return new PrinterStatus("FINISHED"); } }
+        public static PrinterStatus Stopped { get { return new PrinterStatus("STOPPED"); } }
+        public static PrinterStatus Error { get { return new PrinterStatus("ERROR"); } }
+        public static PrinterStatus Attention { get { return new PrinterStatus("ATTENTION"); } }
+        public static PrinterStatus Ready { get { return new PrinterStatus("READY"); } }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            var item = obj as PrinterStatus;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return Value.Equals(item.Value);
+        }
+
+        public static bool operator == (PrinterStatus a, PrinterStatus b)
+        {
+            if (a is null)
+            {
+                return b is null;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator != (PrinterStatus a, PrinterStatus b)
+        {
+            return !(a == b);
+        }
+    }
+}
--- PrusaPushDispatcher/PrusaPushDispatcher.cs
+++ PrusaPushDispatcher/PrusaPushDispatcher.cs
@@ -1,12 +1,72 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace PrusaPushDispatcher
 {
     internal class PrusaPushDispatcher : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        private readonly Settings _settings;
+        private readonly ILogger<PrusaPushDispatcher> _logger;
+
+        public PrusaPushDispatcher(IOptions<Settings> settings,
+            ILogger<PrusaPushDispatcher> logger)
         {
-            throw new NotImplementedException();
+            _settings = settings.Value;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Starting application.");
+
+            if (string.IsNullOrWhiteSpace(_settings.PushoverUserKey))
+            {
+                throw new ArgumentNullException("PushoverUserKey", "The user key was not specified!");
+            }
+
+            if (string.IsNullOrWhiteSpace(_settings.PushoverAppKey))
+            {
+                throw new ArgumentNullException("PushoverAppKey", "The app key was not specified!");
+            }
+
+            using var client = new HttpClient();
+
+            var lastPoll = DateTimeOffset.Now;
+            var status = PrinterStatus.Unknown;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Polling Printer.");
+
+                // check
+
+                if (true)
+                {
+                    _logger.LogInformation("Found new printer status: {status}.", status);
+
+                    var notification = new Dictionary<string, string>
+                    {
+                        { "token", _settings.PushoverAppKey },
+                        { "user", _settings.PushoverUserKey },
+                        { "title", "Printer Status" },
+                        { "message", $"Printer status is now: {status}" }
+                    };
+
+                    await client.PostAsync(
+                            "https://api.pushover.net/1/messages.json",
+                            new FormUrlEncodedContent(notification),
+                            stoppingToken);
+
+                    _logger.LogInformation("Notification pushed.");
+                }
+
+                _logger.LogDebug("Waiting until next poll time: {pollTime}",
+                    DateTimeOffset.Now.AddMinutes(_settings.PollRateInMinutes));
+
+                lastPoll = DateTimeOffset.Now;
+                await Task.Delay(_settings.PollRateInMinutes * 60000, stoppingToken);
+            }
         }
     }
 }
