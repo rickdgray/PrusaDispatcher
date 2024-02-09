9cdef0aad50d1d284cd17fba0489a7be6752e2d5
Rick Gray <rickdgray@outlook.com>
2024-02-09 16:15:59 -0600

    scaffolding

--- PrusaPushDispatcher/Program.cs
+++ PrusaPushDispatcher/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PrusaPushDispatcher;
+
+var builder = Host.CreateApplicationBuilder();
+builder.Logging.AddConsole();
+builder.Services.AddHostedService<PrusaPushDispatcher.PrusaPushDispatcher>();
+builder.Services.Configure<Settings>(
+    builder.Configuration.GetSection(nameof(Settings)));
+
+builder.Build().Run();
---
+++ PrusaPushDispatcher/PrusaPushDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace PrusaPushDispatcher
+{
+    internal class PrusaPushDispatcher : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
