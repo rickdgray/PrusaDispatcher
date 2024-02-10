1b56f99c93a5a812afaf5bf6c401066d15f96987
Rick Gray <rickdgray@outlook.com>
2024-02-10 12:31:35 -0600

    add printer auth model

--- PrusaPushDispatcher/Settings.cs
+++ PrusaPushDispatcher/Settings.cs
@@ -4,6 +4,8 @@
     {
         public string PushoverUserKey { get; set; } = string.Empty;
         public string PushoverAppKey { get; set; } = string.Empty;
+        public string PrinterUsername {  get; set; } = string.Empty;
+        public string PrinterApiKey { get; set; } = string.Empty;
         public string PrinterUrl {  get; set; } = string.Empty;
         public int PollRateInMinutes { get; set; }
     }
